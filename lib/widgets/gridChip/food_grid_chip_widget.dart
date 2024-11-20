203af4e4335a66d33d3d3f19d170ab0372c785ff
WALEED AHMED <waleed.ahmed@appedology.com>
2024-11-21 03:25:57 +0500

    21/11

--- lib/widgets/gridChip/food_grid_chip_widget.dart
+++ lib/widgets/gridChip/food_grid_chip_widget.dart
@@ -9,82 +9,68 @@ import '../../utils/custom_colors.dart';
 import '../../utils/custom_font_style.dart';
 
 Widget foodGridChipWidget({required RecipeModel recipeDetail}) {
-  return Card(
-      elevation: 2,
-      color: CustomColors.whiteColor,
-      borderOnForeground: false,
-      shape: const RoundedRectangleBorder(
-        // side: BorderSide(color: CustomColors.whiteColor),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-                width: double.infinity,
-                decoration: ShapeDecoration(
-                  color: CustomColors.lightGreyColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                child: Stack(
-                  children: [
-                    Center(
-                        child: Image.network(
-                          loadingBuilder: (BuildContext context, Widget child,
-                              ImageChunkEvent? loadingProgress) {
-                            if (loadingProgress == null) return child;
-                            return SizedBox(
-                              height: 108.h,
-                              width: 70.w,
-                              child: Center(
-                                child: LinearProgressIndicator(
-                                  value: loadingProgress.expectedTotalBytes != null
-                                      ? loadingProgress.cumulativeBytesLoaded /
-                                      loadingProgress.expectedTotalBytes!
-                                      : null,
-                                ),
-                              ),
-                            );
-                          },
-                      recipeDetail.media![0],
-                      height: 108.h,
-                    )),
-                    Visibility(
-                      visible: recipeDetail.ingredient!.isNotEmpty,
-                      child: InkWell(
-                        onTap: () {
-                          //TODO: CHANGE RECIPE MODEL
-                          recipeDetailBottomSheetWidget(
-                              recipeDetail: recipeDetail);
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Align(
-                              alignment: Alignment.topRight,
-                              child: SvgPicture.asset(informationButton)),
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0),
-              child: Column(
-                children: [
-                  black14w500(data: recipeDetail.name!, centre: true),
-                  SizedBox(
-                    height: 5.h,
-                  ),
-                  black14w500(data: '(${recipeDetail.lifeStage!})'),
-                ],
-              ),
-            )
-          ],
+  return InkWell(
+    onTap: (){
+      recipeDetailBottomSheetWidget(
+          recipeDetail: recipeDetail);
+    },
+    child: Card(
+        elevation: 2,
+        color: CustomColors.whiteColor,
+        borderOnForeground: false,
+        shape: const RoundedRectangleBorder(
+          // side: BorderSide(color: CustomColors.whiteColor),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
         ),
-      ));
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  width: double.infinity,
+                  decoration: ShapeDecoration(
+                    color: CustomColors.lightGreyColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Center(
+                      child: Image.network(
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent? loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return SizedBox(
+                            height: 108.h,
+                            width: 70.w,
+                            child: Center(
+                              child: LinearProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes != null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                    loadingProgress.expectedTotalBytes!
+                                    : null,
+                              ),
+                            ),
+                          );
+                        },
+                    recipeDetail.media![0],
+                    height: 108.h,
+                  ))),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: Column(
+                  children: [
+                    black14w500(data: recipeDetail.name!, centre: true),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    black14w500(data: '(${recipeDetail.lifeStage!})'),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )),
+  );
 }
